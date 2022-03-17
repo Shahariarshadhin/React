0efce9920f7cbd20ea11df9d3fefa5c5b8d5dd91
shahriarshadhin <shadhin19shahriar@gmail.com>
2022-03-17 12:04:33 +0600

    finish counter master

--- count-master/src/App.js
+++ count-master/src/App.js
@@ -1,25 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Counter></Counter>
+      <Externalusers></Externalusers>
+
+
+
     </div>
   );
+}
+
+/* function Counter() {
+
+  const [count, setCount] = useState(0);
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+
+  return (
+    <div className='counter'>
+      <h1>Count : {count}</h1>
+      <button className='increaseBtn' onClick={increaseCount}>Increase</button>
+      <button className='decreaseBtn' onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+} */
+
+function Counter() {
+
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+
+  return (
+    <div className='counter'>
+      <h1>Count : {count}</h1>
+      <button className='increaseBtn' onClick={increaseCount}>Increase</button>
+      <button className='decreaseBtn' onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+function Externalusers() {
+
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUser(data))
+  }, [])
+  return (
+    <div>
+      <h2>External User</h2>
+      <p>{user.length}</p>
+      {
+        user.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return (
+    <div>
+      <h2>Name : {props.name}</h2>
+      <p>Email: {props.email}</p>
+
+    </div>
+  )
 }
 
 export default App;
